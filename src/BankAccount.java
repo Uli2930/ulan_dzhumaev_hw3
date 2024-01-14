afc60dc59a62796e618f59cac54413ff3be1737b
Amin_stors <uli2930tong@gmail.com>
2024-01-14 20:53:43 +0300

    hw3

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class BankAccount {
+public class BankAccount {
+
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void deposit(double sum) {
+        amount += sum;
+    }
+
+    public void withDraw(int sum) throws LimitException {
+        if (sum > amount) {
+            throw new LimitException("Запрашиваемая сумма на снятие больше," +
+                    " чем остаток на счёте. На вашем счёте осталось: " + this.amount + " сом.", this.amount);
+        }
+        amount -= sum;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+
+        BankAccount bankAccount = new BankAccount();
+
+        bankAccount.deposit(15000);
+
+        System.out.println("У вас на счёте: " + bankAccount.getAmount() + " сом.");
+
+        while (true) {
+            try {
+                bankAccount.withDraw(6000);
+                System.out.println("Указанная вами сумма успешно снята со счёта! \nУ вас на счёте: " + bankAccount.getAmount() + " сом.");
+            } catch (LimitException e) {
+                System.out.println(e.getMessage());
+                try {
+                    bankAccount.withDraw((int) bankAccount.getAmount());
+                } catch (LimitException ex) {
+                    throw new RuntimeException(ex);
+                }
+                System.out.println("Оставшаяся сумма успешно снята со счёта!");
+                System.out.println("Остаток на счёте: " + bankAccount.getAmount() + " сом.");
+                break;
+            }
+        }
+    }
 }
